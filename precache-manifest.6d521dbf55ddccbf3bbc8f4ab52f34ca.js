0535bc9912df9044912d07a4472ad4d3f85c52ad
Christopher Stillman <christopher.stillman@gmail.com>
2020-04-28 18:14:53 -0700

    Updates

---
+++ precache-manifest.6d521dbf55ddccbf3bbc8f4ab52f34ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351875ebcf21b6263dd3857c228b46ff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a34c641ff30574acb2fb",
+    "url": "/monsters-rolodex/static/css/main.1941cc3e.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34c641ff30574acb2fb",
+    "url": "/monsters-rolodex/static/js/main.71e103c3.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
